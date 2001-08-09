f5b57b13ae8a221c35781edc0e8c3ba468edca35
bcurtis <bcurtis>
2001-08-09 13:21:58 +0000

    Got a little crazy with sed

--- admin/user.php
+++ admin/user.php
@@ -1,6 +1,35 @@
 <?php
 
+// user.php - Create and update users
+// ------------------------------------------------------------------------
+// Copyright (c) 2001 The phpBugTracker Group
+// ------------------------------------------------------------------------
+// This file is part of phpBugTracker
+//
+// phpBugTracker is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// phpBugTracker is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with phpBugTracker; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// ------------------------------------------------------------------------
+
 include '../include.php';
+
+function do_form($userid = 0) {
+	global $q, $me, $ffirstname, $flastname, $femail, $fpassword, $usertype, $STRING, $now;
+	
+	// Validation
+	if (!valid_email($femail))
+		$error = $STRING['giveemail'];
+	elseif (!$fpassword = trim($fpassword))
 		$error = $STRING['givepassword'];
 	if ($error) { list_items($userid, $error); return; }
 	
--- bug.php
+++ bug.php
@@ -1,6 +1,33 @@
 <?php
 
-// bug.php
+// bug.php - All the interactions with a bug
+// ------------------------------------------------------------------------
+// Copyright (c) 2001 The phpBugTracker Group
+// ------------------------------------------------------------------------
+// This file is part of phpBugTracker
+//
+// phpBugTracker is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// phpBugTracker is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with phpBugTracker; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// ------------------------------------------------------------------------
+
+include 'include.php';
+
+///
+/// Show the activity for a bug
+function show_history($bugid) {
+	global $q, $t, $STRING;
+	
 	if (!is_numeric($bugid)) {
 		show_text($STRING['nobughistory']);
 		return;
