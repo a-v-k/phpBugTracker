2c95be287c1f827a7770dff11ae6e9f466d334b2
bcurtis <bcurtis>
2001-08-20 22:56:30 +0000

    Starting to add the bug list column preferences

--- user.php
+++ user.php
@@ -23,6 +23,14 @@
 
 include 'include.php';
 
+function change_bug_list_columns($column_list) {
+	global $q, $u, $t;
+	
+	$column_list = serialize($column_list);
+	$q->query("update user set bug_list_fields = '$column_list' where user_id = $u");
+	$t->set_file('content', 'columnlistchanged.html');
+}
+
 function change_password($pass1, $pass2) {
 	global $t, $q, $u, $STRING;
 	
@@ -44,22 +52,26 @@ function change_password($pass1, $pass2) {
 	$t->set_file('content', 'passwordchanged.html');
 }
 
-function show_password_form($error = '') {
+function show_preferences_form($error = '') {
 	global $t, $pass1, $pass2;
 	
-	$t->set_file('content', 'passwordform.html');
+	$t->set_file('content', 'user.html');
 	$t->set_var(array(
 		'error' => $error ? $error.'<br><br>' : '',
 		'pass1' => $pass1,
 		'pass2' => $pass2
 		));
+	
 }
 
 $t->set_file('wrap', 'wrap.html');
 $perm->check('User');
 
-if ($do) change_password($pass1, $pass2);
-else show_password_form();
+if ($do) switch ($op) {
+	case 'changepassword' : change_password($_gv['pass1'], $_gv['pass2']); break;
+	case 'changecolumnlist' : change_bug_list_columns($_gv['column_list']); break;
+	default : show_preferences_form();
+else show_preferences_form();
 
 $t->pparse('main', array('content', 'wrap', 'main'));
 
