bf6f1cf55d857fb43561a3b35ed8c40e4875ada8
bcurtis <bcurtis>
2001-09-03 20:22:42 +0000

    Added created_by and created_date fields

--- perms.sql
+++ perms.sql
@@ -1,8 +1,8 @@
 create table auth_group (group_id int unsigned not null, group_name varchar(80) not null, created_by int unsigned not null, created_date bigint unsigned not null, last_modified_by int unsigned not null, last_modified_date bigint unsigned not null, primary key (group_id));
 create table auth_perm (perm_id int unsigned not null, perm_name varchar(80) not null, created_by int unsigned not null, created_date bigint unsigned not null, last_modified_by int unsigned not null, last_modified_date bigint unsigned not null, primary key (perm_id));
-create table user_group (user_id int unsigned not null, group_id int unsigned not null, primary key (user_id, group_id), key (group_id));
+create table user_group (user_id int unsigned not null, group_id int unsigned not null, created_by int unsigned not null, created_date bigint unsigned not null, primary key (user_id, group_id), key (group_id));
 create table user_perm (user_id int unsigned not null, perm_id int unsigned not null, primary key (user_id, perm_id), key (perm_id));
-create table group_perm (group_id int unsigned not null, perm_id int unsigned not null, primary key (group_id, perm_id), key (perm_id));
+create table group_perm (group_id int unsigned not null, perm_id int unsigned not null, created_by int unsigned not null, created_date bigint unsigned not null, primary key (group_id, perm_id), key (perm_id));
 create table bug_group (bug_id int unsigned not null, group_id int unsigned not null, primary key (bug_id, group_id), key (group_id));
 create table project_group (project_id int unsigned not null, group_id int unsigned not null, primary key (project_id, group_id), key (group_id));
 
@@ -16,11 +16,12 @@ insert into auth_perm (perm_id, perm_name) values (1, 'Admin');
 insert into auth_perm (perm_id, perm_name) values (2, 'Editbug');
 
 # Admins can do all the admin stuff and users can edit bugs
-insert into group_perm (group_id, perm_id) values (1, 1);
-insert into group_perm (group_id, perm_id) values (2, 2);
+insert into group_perm (group_id, perm_id) values (1, 1, 0, 0);
+insert into group_perm (group_id, perm_id) values (2, 2, 0, 0);
 
-# And user_id 1 is the admin
-insert into user_group (user_id, group_id) values (1, 1);
+# And user_id 1 is the admin and a user
+insert into user_group (user_id, group_id) values (1, 1, 0, 0);
+insert into user_group (user_id, group_id) values (1, 2, 0, 0);
 
 # You can use these queries to convert the post 0.2.x / pre 0.3.0 schema
 alter table user rename auth_user;
