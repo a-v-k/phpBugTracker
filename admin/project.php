79d0a27800fe78d68d70769d27eac7576a2d81ee
bcurtis <bcurtis>
2001-10-02 23:17:16 +0000

    Fixes bug #467196 Needed some quotes around the version field on database update

--- admin/project.php
+++ admin/project.php
@@ -20,7 +20,7 @@
 // along with phpBugTracker; if not, write to the Free Software Foundation,
 // Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 // ------------------------------------------------------------------------
-// $Id: project.php,v 1.19 2001/10/02 03:41:19 bcurtis Exp $
+// $Id: project.php,v 1.20 2001/10/02 23:17:16 bcurtis Exp $
 
 define('INCLUDE_PATH', '../');
 include INCLUDE_PATH.'include.php';
@@ -41,7 +41,7 @@ function save_version($versionid = 0) {
 			values (".$q->nextid(TBL_VERSION).", $projectid, '$vf_version', $vf_active, $u, $now)");
   } else {
     $q->query('update '.TBL_VERSION
-			." set project_id = $projectid, version_name = $vf_version, 
+			." set project_id = $projectid, version_name = '$vf_version', 
 			active = $vf_active where version_id = '$versionid'");
   }
   header("Location: project.php?op=edit&id=$projectid");
