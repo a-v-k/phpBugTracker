43ee1f2bde8fc42a56e4e4e8e60a2e730e921a50
bcurtis <bcurtis>
2001-11-06 04:41:14 +0000

    Forgot the extra headers :)

--- upgrade.php
+++ upgrade.php
@@ -1,6 +1,26 @@
 <?php
 
 // upgrade.php -- Upgrade from the previous version
+// ------------------------------------------------------------------------
+// Copyright (c) 2001 The phpBugTracker Group
+// ------------------------------------------------------------------------
+// This file is part of phpBugTracker
+//
+// phpBugTracker is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// phpBugTracker is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with phpBugTracker; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// ------------------------------------------------------------------------
+// $Id: upgrade.php,v 1.3 2001/11/06 04:41:14 bcurtis Exp $
 
 define ('NO_AUTH', 1);
 include 'include.php';
