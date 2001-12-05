ccace635873b19e640c10fc94f0170d0f95753df
bcurtis <bcurtis>
2001-12-05 04:46:12 +0000

    Changes for 0.6.0

--- dbchanges.sql
+++ dbchanges.sql
@@ -1,1 +1,3 @@
-insert into TBL_CONFIGURATION (varname, varvalue, description, vartype) values ('STRICT_UPDATING', '0', 'Only the bug reporter, bug owner, managers, and admins can change a bug', 'bool');
+alter table auth_group add locked tinyint(1) not null default 0 after group_name;
+update auth_group set locked = 1;
+insert into db_sequence values('auth_group', 3); 
