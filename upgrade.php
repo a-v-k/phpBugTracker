e4808176915b1d6d550568f5d317cb11ae4df34a
bcurtis <bcurtis>
2002-03-18 15:54:39 +0000

    Updated for 0.8.0

--- upgrade.php
+++ upgrade.php
@@ -20,7 +20,7 @@
 // along with phpBugTracker; if not, write to the Free Software Foundation,
 // Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 // ------------------------------------------------------------------------
-// $Id: upgrade.php,v 1.11 2002/03/17 01:44:24 bcurtis Exp $
+// $Id: upgrade.php,v 1.12 2002/03/18 15:54:39 bcurtis Exp $
 
 define ('NO_AUTH', 1);
 include 'include.php';
@@ -28,17 +28,24 @@ include 'include.php';
 function upgrade() {
 	global $db;
 	
-	$upgraded = $db->getOne('select varvalue from '.TBL_CONFIGURATION.
-		" where varname = 'PROMOTE_VOTES'");
-	if (!$upgraded) {
-		// Add the bug_vote table and insert the new configuration options
-		if (DB_TYPE == 'pgsql') {
-			$db->query("CREATE TABLE ".TBL_BUG_VOTE." ( user_id INT4  NOT NULL DEFAULT '0', bug_id INT4  NOT NULL DEFAULT '0', created_date INT8  NOT NULL DEFAULT '0', PRIMARY KEY  (user_id,bug_id) );"); 
-		} else {
-			$db->query("create table ".TBL_BUG_VOTE." ( user_id int(10) unsigned NOT NULL default '0', bug_id int(10) unsigned NOT NULL default '0', created_date bigint(20) unsigned NOT NULL default '0', PRIMARY KEY  (user_id, bug_id), KEY bug_id (bug_id) )");
+	$upgraded = $db->getOne('select count(*) from '.TBL_BUG.'_seq');
+	if (!$upgraded or DB::isError($upgraded)) {
+		// Convert the sequences
+		if (DB_TYPE == 'mysql') {
+			// Just in case we have someone who started using phpbt a long time ago...
+			$db->query('update '.TBL_DB_SEQUENCE.' set seq_name = lower(seq_name)');
 		}
-		$db->query("INSERT INTO ".TBL_CONFIGURATION." VALUES ('PROMOTE_VOTES', 5, 'The number of votes required to promote a bug from Unconfirmed to New (Set to 0 to disable promotions by voting)', 'string')");
-		$db->query("INSERT INTO ".TBL_CONFIGURATION." VALUES ('MAX_USER_VOTES', 5, 'The maximum number of votes a user can cast across all bugs (Set to 0 to have no limit)', 'string')");
+		$rs = $db->query("select * from ".TBL_DB_SEQUENCE);
+		if (DB_TYPE == 'pgsql') {
+			while ($rs->fetchInto($row)) {
+				$db->query("create sequence {$row['seq_name']}_seq start {$row['nextid']}");
+			}
+		} else {
+			while ($rs->fetchInto($row)) {
+				$db->query("create table {$row['seq_name']}_seq (id int unsigned auto_increment not null primary key)");
+				$db->query("insert into {$row['seq_name']}_seq values ({$row['nextid']})");
+			}
+		}
 	}
 	include 'templates/default/upgrade-finished.html';
 }
