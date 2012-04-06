f9ebb397bd0a5327d51a13c7877535af6a44126a
Andrey Koryazhkin <avk@digicraft.ru>
2012-04-06 21:09:50 +0400

    change installed db version const

--- install.php
+++ install.php
@@ -262,7 +262,7 @@ function create_tables() {
     }
     /* !! BAD! Must figure out how to get db_version from config-dist.php... */
     /* !! Must manually be updated when DB_VERSION changes !! */
-    $query = preg_replace(array_keys($tables), array_values($tables), 'INSERT INTO TBL_CONFIGURATION (varname,varvalue,description,vartype) VALUES (\'DB_VERSION\', ' . /* !!! */6/* !!! */ . ', \'Database Version <b>Warning:</b> Changing this might make things go horribly wrong, so do not change it.\', \'mixed\')');
+    $query = preg_replace(array_keys($tables), array_values($tables), 'INSERT INTO TBL_CONFIGURATION (varname,varvalue,description,vartype) VALUES (\'DB_VERSION\', ' . /* !!! */7/* !!! */ . ', \'Database Version <b>Warning:</b> Changing this might make things go horribly wrong, so do not change it.\', \'mixed\')');
     log_query($query);
 
     if ($num_errors > 0) {
