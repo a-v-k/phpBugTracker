563fe47cae94749a28343985d05f7771d06b533e
Andrey Koryazhkin <avk@digicraft.ru>
2012-11-08 14:20:03 +0400

    code format

--- templates/default/admin/projectlist.html.php
+++ templates/default/admin/projectlist.html.php
@@ -2,8 +2,9 @@
     <tr>
         <td valign="top">
             <b>&nbsp;<?php echo $page_title; ?></b>
-            <?php if (isset($perm) and $perm->have_perm('Admin'))
-            echo " - <a href=\"{$_SERVER['SCRIPT_NAME']}?op=add\">".translate("Add new project")."</a>";
+            <?php
+            if (isset($perm) and $perm->have_perm('Admin'))
+                echo " - <a href=\"{$_SERVER['SCRIPT_NAME']}?op=add\">" . translate("Add new project") . "</a>";
             ?>
             <hr size="1">
             <table class="bordertable" align="center">
@@ -13,15 +14,19 @@
                     <th class="<?php echo $headers['active']['class']; ?>"><a href="<?php echo $headers['active']['url']; ?>"><?php echo translate("Active"); ?></a></th>
                 </tr>
                 <?php for ($i = 0, $count = count($projects); $i < $count; $i++) { ?>
-                <tr>
-                    <td>
-                        <?php if (isset($perm) and ($perm->have_perm('Admin') or $perm->have_perm_proj($projects[$i]['project_id']))) { ?>
-                        <a href="<?php echo $_SERVER['SCRIPT_NAME']; ?>?op=edit&id=<?php echo $projects[$i]['project_id']; ?>"><?php echo stripslashes($projects[$i]['project_name']); ?></a>
-                        <?php } else { echo stripslashes($projects[$i]['project_name']); } ?>
-                    </td>
-                    <td align="center"><?php echo date(DATE_FORMAT, $projects[$i]['created_date']); ?></td>
-                    <td align="center"><?php echo $projects[$i]['active'] ? translate("Yes") : translate("No"); ?></td>
-                </tr>
+                    <tr>
+                        <td>
+                            <?php if (isset($perm) and ($perm->have_perm('Admin') or $perm->have_perm_proj($projects[$i]['project_id']))) { ?>
+                                <a href="<?php echo $_SERVER['SCRIPT_NAME']; ?>?op=edit&id=<?php echo $projects[$i]['project_id']; ?>"><?php echo stripslashes($projects[$i]['project_name']); ?></a>
+                                <?php
+                            } else {
+                                echo stripslashes($projects[$i]['project_name']);
+                            }
+                            ?>
+                        </td>
+                        <td align="center"><?php echo date(DATE_FORMAT, $projects[$i]['created_date']); ?></td>
+                        <td align="center"><?php echo $projects[$i]['active'] ? translate("Yes") : translate("No"); ?></td>
+                    </tr>
                 <?php } ?>
             </table>
             <?php include('pagination.html'); ?>
