6b3d1d601c7713d2a6901626fc4071a5ad253ec3
Andrey Koryazhkin <avk@digicraft.ru>
2014-05-31 11:41:15 +0400

    code format

--- admin/resolution.php
+++ admin/resolution.php
@@ -27,101 +27,107 @@ define('TEMPLATE_PATH', 'admin');
 include 'include.php';
 
 function del_item($resolutionid = 0) {
-	global $db, $me;
-	
-	if ($resolutionid) {
-		// Make sure we are going after a valid record
-		$itemexists = $db->getOne('select count(*) from '.TBL_RESOLUTION." where resolution_id = $resolutionid");
-		// Are there any bugs tied to this one?
-		$bugcount = $db->getOne('select count(*) from '.TBL_BUG." where resolution_id = $resolutionid");
-		if ($itemexists and !$bugcount) {
-			$db->query('delete from '.TBL_RESOLUTION." where resolution_id = $resolutionid");
-		}
-	}
-	header("Location: $me?");
+    global $db, $me;
+
+    if ($resolutionid) {
+        // Make sure we are going after a valid record
+        $itemexists = $db->getOne('select count(*) from ' . TBL_RESOLUTION . " where resolution_id = $resolutionid");
+        // Are there any bugs tied to this one?
+        $bugcount = $db->getOne('select count(*) from ' . TBL_BUG . " where resolution_id = $resolutionid");
+        if ($itemexists and !$bugcount) {
+            $db->query('delete from ' . TBL_RESOLUTION . " where resolution_id = $resolutionid");
+        }
+    }
+    header("Location: $me?");
 }
 
 function do_form($resolutionid = 0) {
-	global $db, $me, $t;
+    global $db, $me, $t;
 
-	extract($_POST);
-	$error = '';
-	// Validation
-	if (!$resolution_name = trim($resolution_name))
-		$error = translate("Please enter a name");
-	elseif (!$resolution_desc = trim($resolution_desc))
-		$error = translate("Please enter a description");
-	if ($error) { show_form($resolutionid, $error); return; }
+    extract($_POST);
+    $error = '';
+    // Validation
+    if (!$resolution_name = trim($resolution_name))
+        $error = translate("Please enter a name");
+    elseif (!$resolution_desc = trim($resolution_desc))
+        $error = translate("Please enter a description");
+    if ($error) {
+        show_form($resolutionid, $error);
+        return;
+    }
 
-	if (empty($sort_order)) $sort_order = 0;
-	if (!$resolutionid) {
-		$db->query("insert into ".TBL_RESOLUTION.
-			" (resolution_id, resolution_name, resolution_desc, sort_order) values (".$db->nextId(TBL_RESOLUTION).", ".$db->quote(stripslashes($resolution_name)).', '.$db->quote(stripslashes($resolution_desc)).', '.$sort_order.')');
-	} else {
-		$db->query("update ".TBL_RESOLUTION.
-			' set resolution_name = '.$db->quote(stripslashes($resolution_name)).', resolution_desc = '.$db->quote(stripslashes($resolution_desc)).", sort_order = $sort_order where resolution_id = $resolutionid");
-	}
-	if ($use_js) {
-		$t->render('edit-submit.html');
-	} else {
-		header("Location: $me?");
-	}
+    if (empty($sort_order))
+        $sort_order = 0;
+    if (!$resolutionid) {
+        $db->query("insert into " . TBL_RESOLUTION .
+                " (resolution_id, resolution_name, resolution_desc, sort_order) values (" . $db->nextId(TBL_RESOLUTION) . ", " . $db->quote(stripslashes($resolution_name)) . ', ' . $db->quote(stripslashes($resolution_desc)) . ', ' . $sort_order . ')');
+    } else {
+        $db->query("update " . TBL_RESOLUTION .
+                ' set resolution_name = ' . $db->quote(stripslashes($resolution_name)) . ', resolution_desc = ' . $db->quote(stripslashes($resolution_desc)) . ", sort_order = $sort_order where resolution_id = $resolutionid");
+    }
+    if ($use_js) {
+        $t->render('edit-submit.html');
+    } else {
+        header("Location: $me?");
+    }
 }
 
 function show_form($resolutionid = 0, $error = '') {
-	global $db, $me, $t;
+    global $db, $me, $t;
 
-	extract($_POST);
-	if ($resolutionid && !$error) {
-		$t->assign($db->getRow("select * from ".TBL_RESOLUTION." where resolution_id = '$resolutionid'"));
-	} else {
- 		$t->assign($_POST);
-	}
-	$t->assign('error', $error);
-	$t->render('resolution-edit.html', translate("Edit Resolution"),
-		!empty($_REQUEST['use_js']) ? 'wrap-popup.html' : 'wrap.html');
+    extract($_POST);
+    if ($resolutionid && !$error) {
+        $t->assign($db->getRow("select * from " . TBL_RESOLUTION . " where resolution_id = '$resolutionid'"));
+    } else {
+        $t->assign($_POST);
+    }
+    $t->assign('error', $error);
+    $t->render('resolution-edit.html', translate("Edit Resolution"), !empty($_REQUEST['use_js']) ? 'wrap-popup.html' : 'wrap.html');
 }
 
-
 function list_items($resolutionid = 0, $error = '') {
-	global $me, $db, $t, $QUERY;
+    global $me, $db, $t, $QUERY;
 
-	if (empty($_GET['order'])) { 
-		$order = 'sort_order'; 
-		$sort = 'asc'; 
-	} else {
-		$order = $_GET['order']; 
-		$sort = $_GET['sort']; 
-	}
-	
-	$page = isset($_GET['page']) ? $_GET['page'] : 0;
-	
-	$nr = $db->getOne("select count(*) from ".TBL_RESOLUTION);
+    if (empty($_GET['order'])) {
+        $order = 'sort_order';
+        $sort = 'asc';
+    } else {
+        $order = $_GET['order'];
+        $sort = $_GET['sort'];
+    }
 
-	list($selrange, $llimit) = multipages($nr, $page, "order=$order&sort=$sort");
+    $page = isset($_GET['page']) ? $_GET['page'] : 0;
 
-	$t->assign('resolutions', $db->getAll($db->modifyLimitQuery(
-		sprintf($QUERY['admin-list-resolutions'], $order, $sort), $llimit, $selrange)));
+    $nr = $db->getOne("select count(*) from " . TBL_RESOLUTION);
 
-	$headers = array(
-		'resolutionid' => 'resolution_id',
-		'name' => 'resolution_name',
-		'description' => 'resolution_desc',
-		'sortorder' => 'sort_order');
+    list($selrange, $llimit) = multipages($nr, $page, "order=$order&sort=$sort");
 
-	sorting_headers($me, $headers, $order, $sort, "page=$page");
+    $t->assign('resolutions', $db->getAll($db->modifyLimitQuery(
+                            sprintf($QUERY['admin-list-resolutions'], $order, $sort), $llimit, $selrange)));
 
-	$t->render('resolutionlist.html', translate("Resolution List"));
+    $headers = array(
+        'resolutionid' => 'resolution_id',
+        'name' => 'resolution_name',
+        'description' => 'resolution_desc',
+        'sortorder' => 'sort_order');
+
+    sorting_headers($me, $headers, $order, $sort, "page=$page");
+
+    $t->render('resolutionlist.html', translate("Resolution List"));
 }
 
 $perm->check('Admin');
 
 if (isset($_REQUEST['op'])) {
-	switch($_REQUEST['op']) {
-		case 'edit' : show_form($_GET['resolution_id']); break;
-		case 'del' : del_item($_GET['resolution_id']); break;
-		case 'save' : do_form($_POST['resolution_id']); break;
-	}
-} else list_items();
+    switch ($_REQUEST['op']) {
+        case 'edit' : show_form($_GET['resolution_id']);
+            break;
+        case 'del' : del_item($_GET['resolution_id']);
+            break;
+        case 'save' : do_form($_POST['resolution_id']);
+            break;
+    }
+} else
+    list_items();
 
-?>
+//
