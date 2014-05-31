7c81b1fe72dde1a6642b2e1dfbd0df231c2f7788
Andrey Koryazhkin <avk@digicraft.ru>
2014-05-31 12:23:23 +0400

    code format

--- templates/default/wrap-popup.php
+++ templates/default/wrap-popup.php
@@ -1,15 +1,19 @@
-<html>
-<head>
-    <META HTTP-EQUIV="Expires" CONTENT="-1">
-    <title>phpBugTracker - <?php echo $page_title ?></title>
-    <link rel="StyleSheet" href="styles/<?php echo STYLE ?>.css" type="text/css">
-    <META http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET ?>">
-</head>
-<body topmargin="0" leftmargin="0" marginheight="0" marginwidth="0">
-    <table width="100%" cellspacing="0" cellpadding="0">
-	<tr>
-	    <td class="maincontent"><?php include($content_template) ?></td>
-	</tr>
-    </table>
-</body>
+<?php
+if (!defined('PHPBT_VERSION')) {
+    die('not in phpbt');
+}
+?><html>
+    <head>
+        <META HTTP-EQUIV="Expires" CONTENT="-1">
+        <title>phpBugTracker - <?php echo $page_title ?></title>
+        <link rel="StyleSheet" href="styles/<?php echo STYLE ?>.css" type="text/css">
+        <META http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET ?>">
+    </head>
+    <body topmargin="0" leftmargin="0" marginheight="0" marginwidth="0">
+        <table width="100%" cellspacing="0" cellpadding="0">
+            <tr>
+                <td class="maincontent"><?php include($content_template) ?></td>
+            </tr>
+        </table>
+    </body>
 </html>
