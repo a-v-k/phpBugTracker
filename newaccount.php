19628419d6ab75b6fed97ce961d1af1a157557c9
Andrey Koryazhkin <avk@digicraft.ru>
2014-05-31 11:25:57 +0400

    code format

--- newaccount.php
+++ newaccount.php
@@ -23,52 +23,49 @@
 // $Id: newaccount.php,v 1.37 2008/01/28 00:10:40 brycen Exp $
 
 define('NO_AUTH', 1);
-include 'include.php'; 
+include 'include.php';
 
 function do_form() {
-	global $db, $t, $now, $u;
-	
-	if (NEW_ACCOUNTS_DISABLED) {
-		$t->render('newaccount-disabled.html');
-		return;
-	}
+    global $db, $t, $now, $u;
 
-	if (!EMAIL_IS_LOGIN && !$_POST['login'] = trim($_POST['login'])) 
-		$error = translate("Please enter a login");
-	elseif (!$_POST['email'] or !bt_valid_email($_POST['email'])) 
-		$error = translate("Please enter a valid email");
-	elseif ($db->getOne("select user_id from ".TBL_AUTH_USER." where email = '{$_POST['email']}' ".(!empty($_POST['login']) ? "or login = '{$_POST['login']}'" : '')))
-		$error = translate("That login has already been used");
-	if (!empty($error)) { 
-		show_form($error);
-		return;
-	}
-	$firstname = htmlspecialchars($_POST['firstname']);
-	$lastname = htmlspecialchars($_POST['lastname']);
-	$password = genpassword(10);
-	if (ENCRYPT_PASS) {
-		$mpassword = $db->quote(md5($password));
-	} else {
-		$mpassword = $db->quote(stripslashes($password));
-	}
-	if (EMAIL_IS_LOGIN) {
-		$login = $_POST['email'];
-	} else {
-		$login = $_POST['login'];
-	}
-	$user_id = $db->nextId(TBL_AUTH_USER);
+    if (NEW_ACCOUNTS_DISABLED) {
+        $t->render('newaccount-disabled.html');
+        return;
+    }
+
+    if (!EMAIL_IS_LOGIN && !$_POST['login'] = trim($_POST['login']))
+        $error = translate("Please enter a login");
+    elseif (!$_POST['email'] or !bt_valid_email($_POST['email']))
+        $error = translate("Please enter a valid email");
+    elseif ($db->getOne("select user_id from " . TBL_AUTH_USER . " where email = '{$_POST['email']}' " . (!empty($_POST['login']) ? "or login = '{$_POST['login']}'" : '')))
+        $error = translate("That login has already been used");
+    if (!empty($error)) {
+        show_form($error);
+        return;
+    }
+    $firstname = htmlspecialchars($_POST['firstname']);
+    $lastname = htmlspecialchars($_POST['lastname']);
+    $password = genpassword(10);
+    if (ENCRYPT_PASS) {
+        $mpassword = $db->quote(md5($password));
+    } else {
+        $mpassword = $db->quote(stripslashes($password));
+    }
+    if (EMAIL_IS_LOGIN) {
+        $login = $_POST['email'];
+    } else {
+        $login = $_POST['login'];
+    }
+    $user_id = $db->nextId(TBL_AUTH_USER);
     // Change this line to make new member-created accounts inactive.
-	$db->query("insert into ".TBL_AUTH_USER." (user_id, login, first_name, last_name, email, password, active, created_date, last_modified_date) values (".join(', ', array($user_id, $db->quote(stripslashes($login)), $db->quote(stripslashes($firstname)), $db->quote(stripslashes($lastname)), $db->quote($_POST['email']), $mpassword, 1, $now, $now)).")");
-	$db->query("insert into ".TBL_USER_GROUP." (user_id, group_id, created_by, created_date) select $user_id, group_id, 0, $now from ".TBL_AUTH_GROUP." where group_name = '".NEW_ACCOUNTS_GROUP."'"); 
-	$db->query("insert into ".TBL_USER_PREF." (user_id) values ($user_id)");
-	
-	mass_mail4($_POST['email'],
-		translate("phpBugTracker Login"), 
-		sprintf(translate("Your phpBugTracker password is %s"), $password),
-		ADMIN_EMAIL);
+    $db->query("insert into " . TBL_AUTH_USER . " (user_id, login, first_name, last_name, email, password, active, created_date, last_modified_date) values (" . join(', ', array($user_id, $db->quote(stripslashes($login)), $db->quote(stripslashes($firstname)), $db->quote(stripslashes($lastname)), $db->quote($_POST['email']), $mpassword, 1, $now, $now)) . ")");
+    $db->query("insert into " . TBL_USER_GROUP . " (user_id, group_id, created_by, created_date) select $user_id, group_id, 0, $now from " . TBL_AUTH_GROUP . " where group_name = '" . NEW_ACCOUNTS_GROUP . "'");
+    $db->query("insert into " . TBL_USER_PREF . " (user_id) values ($user_id)");
+
+    mass_mail4($_POST['email'], translate("phpBugTracker Login"), sprintf(translate("Your phpBugTracker password is %s"), $password), ADMIN_EMAIL);
 
 
-	$t->render('newaccountsuccess.html', translate("New account created"));
+    $t->render('newaccountsuccess.html', translate("New account created"));
 }
 
 function show_form($error = '') {
@@ -99,7 +96,10 @@ function show_form($error = '') {
     }
 }
 
-if (isset($_POST['createaccount'])) do_form();
-else show_form();
+if (isset($_POST['createaccount'])) {
+    do_form();
+} else {
+    show_form();
+}
 
-?>
+//
