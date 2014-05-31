919534a93c4c6dfafe223bd602a1254abd7df20a
Andrey Koryazhkin <avk@digicraft.ru>
2014-05-31 11:42:06 +0400

    code format

--- admin/site.php
+++ admin/site.php
@@ -27,99 +27,106 @@ define('TEMPLATE_PATH', 'admin');
 include 'include.php';
 
 function del_item($siteid = 0) {
-	global $db, $me;
-	
-	if ($siteid) {
-		// Make sure we are going after a valid record
-		$itemexists = $db->getOne('select count(*) from '.TBL_SITE.
-			" where site_id = $siteid");
-		// Are there any bugs tied to this one?
-		$bugcount = $db->getOne('select count(*) from '.TBL_BUG.
-			" where site_id = $siteid");
-		if ($itemexists and !$bugcount) {
-			$db->query('delete from '.TBL_SITE." where site_id = $siteid");
-		}
-	}
-	header("Location: $me?");
+    global $db, $me;
+
+    if ($siteid) {
+        // Make sure we are going after a valid record
+        $itemexists = $db->getOne('select count(*) from ' . TBL_SITE .
+                " where site_id = $siteid");
+        // Are there any bugs tied to this one?
+        $bugcount = $db->getOne('select count(*) from ' . TBL_BUG .
+                " where site_id = $siteid");
+        if ($itemexists and !$bugcount) {
+            $db->query('delete from ' . TBL_SITE . " where site_id = $siteid");
+        }
+    }
+    header("Location: $me?");
 }
 
 function do_form($siteid = 0) {
-	global $db, $me, $t;
+    global $db, $me, $t;
 
-	extract($_POST);
-	$error = '';
-	// Validation
-	if (!$site_name = trim($site_name))
-		$error = translate("Please enter a name");
-	if ($error) { show_form($siteid, $error); return; }
+    extract($_POST);
+    $error = '';
+    // Validation
+    if (!$site_name = trim($site_name))
+        $error = translate("Please enter a name");
+    if ($error) {
+        show_form($siteid, $error);
+        return;
+    }
 
-	if (empty($sort_order)) $sort_order = 0;
-	if (!$siteid) {
-		$db->query('insert into '.TBL_SITE.' (site_id, site_name, sort_order) values ('.$db->nextId(TBL_SITE).', '.$db->quote(stripslashes($site_name)).', '.$sort_order.')');
-	} else {
-		$db->query('update '.TBL_SITE.' set site_name = '.$db->quote(stripslashes($site_name)).', sort_order = '.$sort_order.' where site_id = '.$site_id);
-	}
-	if ($use_js) {
-		$t->render('edit-submit.html');
-	} else {
-		header("Location: $me?");
-	}
+    if (empty($sort_order))
+        $sort_order = 0;
+    if (!$siteid) {
+        $db->query('insert into ' . TBL_SITE . ' (site_id, site_name, sort_order) values (' . $db->nextId(TBL_SITE) . ', ' . $db->quote(stripslashes($site_name)) . ', ' . $sort_order . ')');
+    } else {
+        $db->query('update ' . TBL_SITE . ' set site_name = ' . $db->quote(stripslashes($site_name)) . ', sort_order = ' . $sort_order . ' where site_id = ' . $site_id);
+    }
+    if ($use_js) {
+        $t->render('edit-submit.html');
+    } else {
+        header("Location: $me?");
+    }
 }
 
 function show_form($siteid = 0, $error = '') {
-	global $db, $me, $t;
+    global $db, $me, $t;
 
-	if ($siteid && !$error) {
-		$t->assign($db->getRow("select * from ".TBL_SITE." where site_id = '$siteid'"));
-	} else {
- 		$t->assign($_POST);
-	}
-	$t->assign('error', $error);
-	$t->render('site-edit.html', translate("Edit Site"), 
-		!empty($_REQUEST['use_js']) ? 'wrap-popup.html' : 'wrap.html');
+    if ($siteid && !$error) {
+        $t->assign($db->getRow("select * from " . TBL_SITE . " where site_id = '$siteid'"));
+    } else {
+        $t->assign($_POST);
+    }
+    $t->assign('error', $error);
+    $t->render('site-edit.html', translate("Edit Site"), !empty($_REQUEST['use_js']) ? 'wrap-popup.html' : 'wrap.html');
 }
 
 function list_items($siteid = 0, $error = '') {
-	global $me, $db, $t, $QUERY;
+    global $me, $db, $t, $QUERY;
 
-	if (empty($_GET['order'])) { 
-		$order = 'sort_order'; 
-		$sort = 'asc'; 
-	} else {
-		$order = $_GET['order']; 
-		$sort = $_GET['sort']; 
-	}
-	
-	$page = isset($_GET['page']) ? $_GET['page'] : 0;
-	
-	$nr = $db->getOne("select count(*) from ".TBL_SITE);
+    if (empty($_GET['order'])) {
+        $order = 'sort_order';
+        $sort = 'asc';
+    } else {
+        $order = $_GET['order'];
+        $sort = $_GET['sort'];
+    }
 
-	list($selrange, $llimit) = multipages($nr, $page, "order=$order&sort=$sort");
+    $page = isset($_GET['page']) ? $_GET['page'] : 0;
 
-	$t->assign('sites', $db->getAll($db->modifyLimitQuery(
-		sprintf($QUERY['admin-list-sites'], $order, $sort), $llimit, $selrange)));
+    $nr = $db->getOne("select count(*) from " . TBL_SITE);
 
-	$headers = array(
-		'siteid' => 'site_id',
-		'name' => 'site_name',
-		'sortorder' => 'sort_order');
+    list($selrange, $llimit) = multipages($nr, $page, "order=$order&sort=$sort");
 
-	sorting_headers($me, $headers, $order, $sort);
+    $t->assign('sites', $db->getAll($db->modifyLimitQuery(
+                            sprintf($QUERY['admin-list-sites'], $order, $sort), $llimit, $selrange)));
 
-	$t->render('sitelist.html', translate("Site List"));
+    $headers = array(
+        'siteid' => 'site_id',
+        'name' => 'site_name',
+        'sortorder' => 'sort_order');
+
+    sorting_headers($me, $headers, $order, $sort);
+
+    $t->render('sitelist.html', translate("Site List"));
 }
 
 $perm->check('Admin');
 
 if (isset($_REQUEST['op'])) {
-    switch($_REQUEST['op']) {
-	case 'add' : list_items(); break;
-	case 'edit' : show_form($_REQUEST['site_id']); break;
-	case 'del' : del_item($_REQUEST['site_id']); break;
-	case 'save' : do_form($_POST['site_id']); break;
+    switch ($_REQUEST['op']) {
+        case 'add' : list_items();
+            break;
+        case 'edit' : show_form($_REQUEST['site_id']);
+            break;
+        case 'del' : del_item($_REQUEST['site_id']);
+            break;
+        case 'save' : do_form($_POST['site_id']);
+            break;
     }
 } else {
     list_items();
 }
 
-?>
+//
