a066c4245b3028fe9a785deb1477cf6307fa02e8
a-v-k <a-v-k@users.noreply.github.com>
2015-02-07 16:30:21 +0400

    code format

---
+++ templates/default/admin/priority-edit.html.php
@@ -0,0 +1,77 @@
+<script type="text/javascript" language="JavaScript">
+    var nameString = '<?php echo translate("Please enter a name"); ?>';
+    var descString = '<?php echo translate("Please enter a description"); ?>';
+
+    function checkForm(frm) {
+        if (frm.priority_name.value == '') {
+            alert(nameString);
+            frm.priority_name.focus();
+            return false;
+        }
+        if (frm.priority_desc.value == '') {
+            alert(descString);
+            frm.priority_desc.focus();
+            return false;
+        }
+        return true;
+    }
+</script>
+<b><?php echo $page_title; ?>&nbsp;</b> 
+<hr size="1">
+<form method="post" onsubmit="return checkForm(this)">
+    <table border='0'>
+        <?php if ($error) { ?>
+            <tr>
+                <td colspan="2" class="error">
+                    <?php echo $error; ?>
+                </td>
+            </tr>
+        <?php } ?>
+        <tr>
+            <td align="right" valign="top">
+                <?php echo translate("Name"); ?>:
+            </td>
+            <td>
+                <input type="text" size="20" maxlength="40" name="priority_name" value="<?php echo stripslashes(htmlspecialchars($priority_name)); ?>">
+            </td>
+        </tr>
+        <tr>
+            <td align="right" valign="top">
+                <?php echo translate("Description"); ?>:
+            </td>
+            <td>
+                <textarea name="priority_desc" cols="20" rows="5" ><?php echo stripslashes(htmlspecialchars($priority_desc)); ?>
+                </textarea>
+            </td>
+        </tr>
+        <tr>
+            <td align="right" valign="top">
+                <?php echo translate("Sort Order"); ?>:
+            </td>
+            <td>
+                <input type="text" size="3" maxlength="3" name="sort_order" value="<?php echo $sort_order; ?>">
+            </td>
+        </tr>
+        <tr>
+            <td align="right" valign="top">
+                <?php echo translate("Row Color"); ?>:
+            </td>
+            <td>
+                <input type="text" size="11" maxlength="10" name="priority_color" value="<?php echo $priority_color; ?>">
+            </td>
+        </tr>
+        <tr>
+            <td align="right" valign="top">
+                &nbsp;
+            </td>
+            <td>
+                <input type='submit' name='submit' value='<?php echo translate("Submit"); ?>'>
+                <input type="hidden" name="priority_id" value="<?php echo $priority_id; ?>"> 
+                <input type="hidden" name="use_js" value="<?php echo $_REQUEST['use_js']; ?>"> 
+                <input type="hidden" name="op" value="save"> 
+            </td>
+        </tr>
+    </table>
+
+    <br>
+</form>
